5f5bb9412e5abc170198b7720ae4a9e47b6c3eaf
Jordy Moorman <noah.james.moorman@gmail.com>
2025-09-17 06:14:13 -0500

    updated index and files for initial working build

---
+++ BibleQuestForKids/App.xaml.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace BibleQuestForKids;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
+    }
+}
